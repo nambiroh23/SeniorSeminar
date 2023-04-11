ccca82a3cbcaa3be0a5aeb27339d02297e22ae18
nambiroh23 <77296224+nambiroh23@users.noreply.github.com>
2023-04-11 00:13:22 -0400

    new block oops

--- Block.java
+++ Block.java
@@ -1,28 +1,28 @@
-class Block {
-  public Class[] rooms = new Class[5];
+class Block { // one block of the day
+  public Class[] rooms = new Class[5]; // what classes are in each of the rooms, 0-4
 
-  public boolean classInBlock(int x) {
+  public boolean classInBlock(int x) { // does this block contain class x?
     for(int i = 0; i<5; i++) {
       if(rooms[i]!=null && rooms[i].id==x) return true;
     }
     return false;
   }
 
-  public int findInBlock(int x) {
+  public int findInBlock(int x) { // find in which room we have class x; -1 of not found
     for(int i = 0; i<5; i++) {
       if(rooms[i].id==x) return i;
     }
     return -1;
   }
 
-  public int nextAvail() {
+  public int nextAvail() { // next available classroom; -1 if none
     for(int i = 0; i<5; i++) {
       if(rooms[i]==null) return i;
     }
     return -1;
   }
 
-  public void addClass(int x) {
+  public void addClass(int x) { // add class with id x
     rooms[nextAvail()] = new Class(x);
   }
 }
